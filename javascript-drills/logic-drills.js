24ee404e9725f85ddade833a6f321e119eb9dd78
Kayla Webb <kaylarwebb@gmail.com>
2017-10-13 19:00:39 -0400

    add items to an array

--- javascript-drills/logic-drills.js
+++ javascript-drills/logic-drills.js
@@ -95,3 +95,45 @@ function testMakeList() {
 }
 
 testMakeList();
+
+
+//////
+
+function addToList(list, item) {
+  list.push(item);
+   return list;
+}
+
+
+// add an item to an array and return to the array
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+
+// tests
+
+function testAddToList() {
+  
+  const input1 = ["red", "blue", "green"];
+  const input2 = "pink";
+  const expected = ["red", "blue", "green", "pink"];
+  const result = addToList(input1, input2);
+  
+  if (
+    result && result.length && expected.length === result.length &&
+    expected.every(function(item) {
+      return result.indexOf(item) > -1;
+    })) {
+    console.log('SUCCESS: `addToList` works!');
+  } else {
+    console.error('FAILURE: `addToList` is not working');
+  } 
+}
+
+testAddToList();
